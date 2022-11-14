abdc06cc7c312991200dd777a6620daecd7acc39
Sean LaPlante <laplante.sean@gmail.com>
2022-11-14 17:29:11 -0500

    Cleaned up main menu

---
+++ Effects/PhysicsDirt.gd
@@ -0,0 +1,6 @@
+extends RigidBody2D
+class_name PhysicsDirt
+
+
+func _on_Timer_timeout():
+    mode = MODE_STATIC
--- Menus/DirtSpawner.gd
+++ Menus/DirtSpawner.gd
@@ -1,18 +1,24 @@
 extends Node2D
+class_name DirtSpawner
 
-const BlockDestroyEffect = preload("res://Effects/LargeBlockParticle.tscn")
+const PhysicsDirtScene = preload("res://Effects/PhysicsDirt.tscn")
 
-export(int) var BULLET_SPEED = 30
+export(int) var MAX_BULLET_SPEED = 80
+export(int) var MIN_BULLET_SPEED = 20
 export(int) var SPREAD = 90
 
 onready var fireDirection = $FireDirection
 onready var dirtSpawnPoint = $DirtSpawnPoint
 
+
 func _on_Timer_timeout():
     spawnDirt()
-    
+
+
 func spawnDirt():
-    var dirt = Utils.instance_scene_on_main(BlockDestroyEffect, dirtSpawnPoint.global_position)
-    var velocity = (fireDirection.global_position - global_position).normalized() * BULLET_SPEED
+    var dirt: RigidBody2D = Utils.instance_scene_on_main(PhysicsDirtScene, dirtSpawnPoint.global_position)
+    var direction = global_position.direction_to(fireDirection.global_position)
+    var speed = rand_range(MIN_BULLET_SPEED, MAX_BULLET_SPEED)
+    var velocity = direction * speed
     velocity = velocity.rotated(deg2rad(rand_range(-SPREAD, SPREAD/2)))
     dirt.apply_central_impulse(velocity)
